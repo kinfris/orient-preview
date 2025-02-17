8bd87ea9f2ecc32da0779796b75f8fb25658c557
Yura Pantelei <jormungand1616@gmail.com>
2025-02-17 11:22:56 +0300

    add active link

--- src/Header/Nav/index.tsx
+++ src/Header/Nav/index.tsx
@@ -2,27 +2,38 @@
 
 import React, { useState } from 'react'
 
-import type { Header as HeaderType, Service } from '@/payload-types'
+import type { Service } from '@/payload-types'
 
 import Link from 'next/link'
 import styles from './nav.module.scss'
 import { Popup } from '@/components/Popup/Popup'
+import { usePathname } from 'next/navigation'
 
 export const HeaderNav: React.FC<{ servicesData: Partial<Service>[] }> = ({ servicesData }) => {
   const [isPopupOpen, setIsPopupOpen] = useState(false)
+  const pathname = usePathname()
 
   return (
     <nav className={styles.navigation}>
-      <Link href="/cases" className={styles.link}>
+      <Link
+        href="/cases"
+        className={`${styles.link} ${pathname === '/cases' ? styles.active : null}`}
+      >
         <span>Cases</span>
       </Link>
-      <Link href="/blogs" className={styles.link}>
+      <Link
+        href="/blogs"
+        className={`${styles.link} ${pathname === '/blogs' ? styles.active : null}`}
+      >
         <span>Blog</span>
       </Link>
       <div className={styles.link} onClick={() => setIsPopupOpen(true)}>
         <span>Services</span>
       </div>
-      <Link href="/contact" className={styles.link}>
+      <Link
+        href="/contact"
+        className={`${styles.link} ${pathname === '/contact' ? styles.active : null}`}
+      >
         <span>Contact Us</span>
       </Link>
       <Popup isOpen={isPopupOpen} setIsOpen={setIsPopupOpen} servicesData={servicesData} />
