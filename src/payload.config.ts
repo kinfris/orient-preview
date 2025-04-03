402c79a47416821e2c4fe3c42b08080a5d6d4c3a
vladosmolovskiy <vladosmolovsky57@gmail.com>
2025-04-03 10:13:24 +0200

    fix s3 config

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -84,10 +84,10 @@ export default buildConfig({
       bucket: process.env.S3_BUCKET!, // Your S3 bucket name
       config: {
         credentials: {
-          accessKeyId: process.env.S3_ACCESS_KEY_ID,
-          secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+          accessKeyId: process.env.S3_ACCESS_KEY_ID!,
+          secretAccessKey: process.env.S3_SECRET_ACCESS_KEY!,
         },
-        region: process.env.S3_REGION,
+        region: process.env.S3_REGION!,
         // ... Other S3 configuration
       },
     }),
