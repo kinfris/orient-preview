78d3457a7e30ee242d01c24330fadee7ab535638
vladosmolovskiy <vladosmolovsky57@gmail.com>
2025-04-03 09:48:30 +0200

    fix

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -81,10 +81,10 @@ export default buildConfig({
           prefix: 'home-prefix',
         },
       },
-      bucket: process.env.S3_BUCKET, // Your S3 bucket name
-      region: process.env.S3_REGION, // e.g., "us-east-1"
-      accessKeyId: process.env.S3_ACCESS_KEY_ID,
-      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+      bucket: process.env.S3_BUCKET!, // Your S3 bucket name
+      region: process.env.S3_REGION!, // e.g., "us-east-1"
+      accessKeyId: process.env.S3_ACCESS_KEY_ID!,
+      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY!,
     }),
     // storage-adapter-placeholder
     // vercelBlobStorage({
