9127e80403aef3645d54215127a240d18782c01f
Yura Pantelei <jormungand1616@gmail.com>
2025-04-10 23:59:24 +0300

    update config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
 import { withPayload } from '@payloadcms/next/withPayload'
-
 import redirects from './redirects.js'
 
 const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
   ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-  : undefined || process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
+  : process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
-      ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
+      // URL твоего сайта (где изображения идут через Payload API, если нужно)
+      ...[NEXT_PUBLIC_SERVER_URL].map((item) => {
         const url = new URL(item)
 
         return {
@@ -18,6 +18,11 @@ const nextConfig = {
           protocol: url.protocol.replace(':', ''),
         }
       }),
+      // URL прямого доступа к S3 изображениям
+      {
+        protocol: 'https',
+        hostname: 'your-bucket-name.s3.eu-central-1.amazonaws.com',
+      },
     ],
   },
   reactStrictMode: true,
