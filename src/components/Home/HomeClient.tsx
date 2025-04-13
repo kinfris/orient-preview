71b8cb7cb8f55bb460f3e0d50e09fb535eb72c82
Yura Pantelei <jormungand1616@gmail.com>
2025-04-13 18:39:24 +0300

    change applying images

--- src/components/Home/HomeClient.tsx
+++ src/components/Home/HomeClient.tsx
@@ -17,6 +17,7 @@ import { CasesSwiper } from '../Sliders/CasesSwiper'
 import { ReviewsSwiper } from '../Sliders/ReivewsSwiper'
 import { TeamSwiper } from '../Sliders/TeamSwiper'
 import { ServicesSwiper } from '../Sliders/ServicesSwiper'
+import Image from 'next/image'
 
 type Props = {
   homeData: Home[]
@@ -52,7 +53,16 @@ export const HomeClient = ({ homeData, casesData, servicesData }: Props) => {
   return (
     <div className={styles.container}>
       <div className={styles.topBgContainer}>
-        <div className={styles.topBg}></div>
+        <div className={styles.topBg}>
+          <Image
+            src="/home_bg.svg"
+            alt="Background"
+            fill
+            priority
+            className={styles.topBgImage}
+            style={{ objectFit: 'cover', objectPosition: 'center', zIndex: -1 }}
+          />
+        </div>
         <div className={styles.topBgGradient}></div>
         <div className={styles.topContentContainer}>
           <div className={styles.topContentWrapper}>
@@ -74,7 +84,16 @@ export const HomeClient = ({ homeData, casesData, servicesData }: Props) => {
       </div>
       <div className={styles.overflowContainer}>
         <div className={styles.casesReviewsWrapper}>
-          <div className={styles.casesReviewsBg}></div>
+          <div className={styles.casesReviewsBg}>
+            <Image
+              src="/home_g_bg.svg"
+              alt="Background"
+              fill
+              priority
+              className={styles.casesReviewsBgImage}
+              style={{ objectFit: 'cover', objectPosition: 'center', zIndex: -1 }}
+            />
+          </div>
           <div className={styles.casesReviewsBgGradient}></div>
           <div className={styles.casesReviewsContainer}>
             <CasesSwiper casesData={casesData} />
